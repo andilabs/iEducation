a782ec3eaab32545944c1f64fe1412ec182825d8
andrzej.kostanski <andrzej.kostanski@daftcode.pl>
2015-01-23 02:28:35 +0100

    initial

---
+++ one-button-app/BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Andrzej Kostanski on 23.01.2015.
+//  Copyright (c) 2015 Andrzej Kostanski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+    }
+
+}
+
